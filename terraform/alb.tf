9cb2498a0ba1227ac938d2183dc31a2b88cdf5f8
Wil Campos <wilbert.campos@gmail.com>
2020-02-09 22:22:00 -0800

    Step

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -61,17 +61,17 @@ resource "aws_lb_target_group" "prometheus-tg" {
   }
 }
 
-# Jenkins
-resource "aws_lb_target_group" "jenkins-tg" {
-  name     = "jenkins-tg"
-  port     = 8080
-  protocol = "HTTP"
-  vpc_id   = module.vpc.vpc_id
-  
-  health_check {
-    matcher  = "200,302"
-  }
-}
+## Jenkins
+#resource "aws_lb_target_group" "jenkins-tg" {
+#  name     = "jenkins-tg"
+#  port     = 8080
+#  protocol = "HTTP"
+#  vpc_id   = module.vpc.vpc_id
+#  
+#  health_check {
+#    matcher  = "200,302"
+#  }
+#}
 
 #--------------------------------------------------------------
 # Target Group Attachments 
@@ -91,12 +91,12 @@ resource "aws_lb_target_group_attachment" "prometheus-tga" {
   port             = 9090
 }
 
-## JENKINS
-resource "aws_lb_target_group_attachment" "jenkins-tga" {
-  target_group_arn = aws_lb_target_group.jenkins-tg.arn
-  target_id        = aws_instance.jenkins-server.id
-  port             = 8080
-}
+### JENKINS
+#resource "aws_lb_target_group_attachment" "jenkins-tga" {
+#  target_group_arn = aws_lb_target_group.jenkins-tg.arn
+#  target_id        = aws_instance.jenkins-server.id
+#  port             = 8080
+#}
 
 #--------------------------------------------------------------
 # Rules
@@ -136,19 +136,19 @@ resource "aws_lb_listener_rule" "prometheus_lbr" {
   }
 }
 
-## JENKINS  
-
-resource "aws_lb_listener_rule" "jenkins_lbr" {
-  listener_arn = aws_lb_listener.https-dashboards.arn
-  
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.jenkins-tg.arn
-  }
-
-  condition {
-    host_header {
-      values = ["jenkins.${var.domain}"]
-    }
-  }
-}
+### JENKINS  
+#
+#resource "aws_lb_listener_rule" "jenkins_lbr" {
+#  listener_arn = aws_lb_listener.https-dashboards.arn
+#  
+#  action {
+#    type             = "forward"
+#    target_group_arn = aws_lb_target_group.jenkins-tg.arn
+#  }
+#
+#  condition {
+#    host_header {
+#      values = ["jenkins.${var.domain}"]
+#    }
+#  }
+#}
